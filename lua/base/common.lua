380a3c820c9af4fc42024c29ab54ed0c131f0fd1
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-28 22:00:21 +0700

    fix(config): fix fatal error for relative number

--- lua/base/common.lua
+++ lua/base/common.lua
@@ -4,12 +4,8 @@ local api = vim.api
 -- [line number]
 -- show line number
 opt.number = true
-
-if api.mode() != 'i' then
-    -- show relative number
-    -- exclude insert mode
-    opt.rnu = true
-end
+-- show relative number
+opt.relativenumber = true
 
 -- [common]
 -- Highlight the screen line of the cursor with CursorLine
