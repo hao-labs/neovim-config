7f7ce2f0f5d32fc1286dd7f2aa245911cc15429b
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-28 23:49:34 +0700

    fix(packer): fix fatal error happen when autocmd implemented inside plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,13 +4,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
