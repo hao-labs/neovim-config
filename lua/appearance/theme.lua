40ddd9b338ff4210247dec11f402177a8a226764
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 18:56:31 +0700

    chore(theme): change theme from nightfox into duskfox

--- lua/appearance/theme.lua
+++ lua/appearance/theme.lua
@@ -1,7 +1,7 @@
 local nightfox = require('nightfox')
 
 nightfox.setup({
-    fox = "nightfox" -- change the colorscheme to use nordfox
+    fox = "duskfox" -- change the colorscheme to use nordfox
 })
 
 -- Load the configuration set above and apply the colorscheme
