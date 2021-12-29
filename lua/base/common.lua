7997b6d2270b67db53e55450abde9e12c24c48d3
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 20:44:46 +0700

    docs(config): enable mouse support and tidy up the documentation

--- lua/base/common.lua
+++ lua/base/common.lua
@@ -1,31 +1,35 @@
 local opt = vim.opt
 local api = vim.api
 
--- [line number]
--- show line number
-opt.number = true
--- show relative number
-opt.relativenumber = true
+-----------------------------------------------------
+-- General
+-----------------------------------------------------
+opt.mouse = 'a'               -- enable mouse support
+opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
+opt.swapfile = false          -- don't use swapfile/temp file
+opt.visualbell = true         -- turn on visual bell
+opt.showmatch = true          -- show matching bracket after last closing
 
--- [common]
--- Highlight the screen line of the cursor with CursorLine
-opt.cursorline = true
--- set visual bells
-opt.visualbell = true
--- show matching bracket after last closing
-opt.showmatch = true
+-----------------------------------------------------
+-- Performance
+-----------------------------------------------------
+opt.hidden = true             -- enable background buffers
+opt.history = 100             -- remember n lines in history
+opt.lazyredraw = true         -- faster scrolling
+opt.synmaxcol = 240           -- max column for syntax hightlight
+opt.ttimeoutlen = 0           -- in miliseconds
+opt.timeoutlen = 200          -- faster delay of waiting key combination when user typing
+opt.timeout = false           -- no waiting for key combination
+
+-----------------------------------------------------
+-- Line Number
+-----------------------------------------------------
+opt.number = true             -- show line number
+opt.relativenumber = true     -- show relative number
+
 
 -- A List is an ordered sequence of items.
--- show indentation
-opt.list = true
-opt.listchars = "tab:-->,trail:·"
--- Minimal number of screen lines to keep above and below the cursor.
-opt.scrolloff = 5
--- Limit timeout waiting time to reduce the delay of waiting key code sequence
-opt.timeoutlen = 200 
-opt.ttimeoutlen = 0
--- no waiting for key combination
-opt.timeout = false
-
--- remember where to recover cursor
-opt.viewoptions = 'cursor,folds,slash,unix'
+opt.list = true               -- show indentation
+opt.listchars = 
+    "tab:-->,trail:·"         -- list of characters 
+opt.scrolloff = 5             -- Minimal number of screen lines to keep above and below the cursor.
