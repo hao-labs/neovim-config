d5ab6820fbadb1bfd68a3824529c749a3a48d648
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 21:51:27 +0700

    refactor(packer): refactor packer configuration into another file

---
+++ lua/utils/packer.lua
@@ -0,0 +1,29 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+      'git', 
+      'clone', 
+      '--depth', 
+      '1', 
+      'https://github.com/wbthomason/packer.nvim', 
+      install_path
+  })
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return packer
