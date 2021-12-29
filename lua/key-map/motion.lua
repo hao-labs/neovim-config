f8deb23d2d78ed90a1f758276cf105b120b8e56c
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 13:14:35 +0700

    chore(mappings): add mappings for split window navigation

---
+++ lua/key-map/motion.lua
@@ -0,0 +1,6 @@
+-- Split window navigation (ctrl + {h,j,k,l})
+map {'n', '<C-h>', '<C-w>h', noremap = true}
+map {'n', '<C-j>', '<C-w>j', noremap = true}
+map {'n', '<C-k>', '<C-w>k', noremap = true}
+map {'n', '<C-l>', '<C-w>l', noremap = true}
+
