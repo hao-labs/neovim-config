9372883e16e7354ce8644c2026b5cc834e2c3fad
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 21:50:24 +0700

    chore(mappings): add key mappings for git status

--- lua/key-map/fuzzy.lua
+++ lua/key-map/fuzzy.lua
@@ -11,3 +11,4 @@ map {'n', 'ff', ':Telescope find_files<cr>'}    -- Find files
 map {'n', 'fif', ':Telescope live_grep<cr>'}    -- Find grep in files
 map {'n', 'ft', ':Telescope buffers<cr>'}       -- Find active tab
 map {'n', 'fh', ':Telescope help_tags<cr>'}     -- Find help
+map {'n', 'gs', ':Telescope git_status<cr>'}    -- Git status of active files
