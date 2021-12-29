97123b6508b54757d051fb641f6b33ad5707590a
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-29 17:42:58 +0700

    feat(plugins): add plugins for fuzzy search, theme and tab

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,36 +1,52 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = fn.system({
+      'git', 
+      'clone', 
+      '--depth', 
+      '1', 
+      'https://github.com/wbthomason/packer.nvim', 
+      install_path
+  })
 end
 
 return require('packer').startup(function(use)
     -- [packer.nvim] Packer can manage itself
     use {"wbthomason/packer.nvim", event = "VimEnter"}
 
-    -- [nvim-web-devicons]
+    -- [nightfox.nvim] Theme 
+    use {'EdenEast/nightfox.nvim'}
+
+    -- [nvim-web-devicons] File Type Icons
     use {'kyazdani42/nvim-web-devicons', event = "BufRead"}
+
+    -- [barbar.nvim] Tab
+    use {'romgrk/barbar.nvim', event = "BufRead"}
 
     -- [nvim-tree.lua] File Explorer
     use {
         'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icon
-        },
         config = function() require'nvim-tree'.setup {} end
     }
 
-    -- telescope: extensible fuzzy file finder--
-    -- use {
-    --     'nvim-telescope/telescope.nvim',
-    --     requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'},
-    --     config = function() require("config.telescope_config") end,
-    --     module = 'telescope'
-    -- }
+    -- [telescope-fzf-native.nvim] extensible fuzzy file finder--
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+    -- [telescope.nvim] extensible fuzzy file finder--
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            'nvim-lua/popup.nvim', 
+            'nvim-lua/plenary.nvim'
+        },
+        module = 'telescope'
+    }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
-        require('packer').sync()
+        require ('packer').sync()
     end
 end)
