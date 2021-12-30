432f0a6bdf86e7ae1371772138e2619c36be76ad
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-30 22:47:46 +0700

    docs(mappings): include keyboard shortcuts for documentation

--- lua/key-map/fuzzy.lua
+++ lua/key-map/fuzzy.lua
@@ -7,8 +7,8 @@ local map = require('utils.map')
 -------------------------------------------------------------------
 -- Find Files, Grep, Active Tab and Help config
 -------------------------------------------------------------------
-map {'n', 'ff', ':Telescope find_files<cr>'}    -- Find files
-map {'n', 'fif', ':Telescope live_grep<cr>'}    -- Find grep in files
-map {'n', 'ft', ':Telescope buffers<cr>'}       -- Find active tab
-map {'n', 'fh', ':Telescope help_tags<cr>'}     -- Find help
-map {'n', 'gs', ':Telescope git_status<cr>'}    -- Git status of active files
+map {'n', '<C-p>', ':Telescope find_files<cr>'} -- ( Ctrl+p ) Find files
+map {'n', 'fif', ':Telescope live_grep<cr>'}    -- ( fif ) Find grep in files
+map {'n', 'ft', ':Telescope buffers<cr>'}       -- ( ft ) Find active tab
+map {'n', 'fh', ':Telescope help_tags<cr>'}     -- ( fh ) Find help
+map {'n', 'gs', ':Telescope git_status<cr>'}    -- ( gs ) Git status of active files
