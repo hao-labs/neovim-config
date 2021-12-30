ec7e4fb04cba1c177c8b68e21b68b07ce1e8ad32
Cahyo Bagus L <hao.labs.2011@gmail.com>
2021-12-30 22:51:50 +0700

    docs(plugins): tidy up documentations to make it more readable

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,42 +1,64 @@
 local packer = require('utils.packer')
 
 return packer.startup(function(use)
-    -- [packer.nvim] Packer can manage itself
-    use {"wbthomason/packer.nvim", event = "VimEnter"}
+    ----------------------------------------------------------------------------------------------
+    -- Plugin Manager
+    ----------------------------------------------------------------------------------------------
+    use {"wbthomason/packer.nvim", event = "VimEnter"}         -- Packer can manage itself
 
-    -- [nightfox.nvim] Theme 
-    use {'EdenEast/nightfox.nvim'}
-
-    -- [nvim-web-devicons] File Type Icons
-    use {'kyazdani42/nvim-web-devicons', event = "BufRead"}
-
-    -- [barbar.nvim] Tab
-    use {'romgrk/barbar.nvim', event = "BufRead"}
-
-    -- [nvim-tree.lua] File Explorer
+    ----------------------------------------------------------------------------------------------
+    -- Appearance/User Interface
+    ----------------------------------------------------------------------------------------------
+    use {'EdenEast/nightfox.nvim'}                             -- Theme 
     use {
-        'kyazdani42/nvim-tree.lua',
-        config = function() require'nvim-tree'.setup {} end
+        'kyazdani42/nvim-web-devicons',                        -- File Type Icons
+        event = "BufRead"
+    }
+    use {'romgrk/barbar.nvim', event = "BufRead"}              -- Tab Management
+    use {
+        'kyazdani42/nvim-tree.lua',                            -- File Explorer 
+        config = function() require'nvim-tree'.setup {} end    -- Git File Indicator
     }
 
-    -- [telescope-fzf-native.nvim] extensible fuzzy file finder--
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
-    -- [telescope.nvim] extensible fuzzy file finder--
     use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
+        'nvim-telescope/telescope.nvim',                       -- Fuzzy Search for file, git, help
+        requires = {                                           -- extensible to other fuzzy process
             'nvim-lua/popup.nvim', 
             'nvim-lua/plenary.nvim'
         }
     }
-
     use {
-        'lewis6991/gitsigns.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim'
+        'nvim-telescope/telescope-fzf-native.nvim',            -- Additional plugins for telescope
+        run = 'make'                                           -- with native fzf
+    }
+
+    ----------------------------------------------------------------------------------------------
+    -- Git
+    ----------------------------------------------------------------------------------------------
+    use {
+        'lewis6991/gitsigns.nvim',                             -- Signs for added, removed, and changed lines
+        requires = {                                           -- Hunk Integration (jump, add to stage, dll)
+            'nvim-lua/plenary.nvim'                            -- Git Blame and preview
         },
-        config = function() require ('gitsigns').setup() end
+        config = function() 
+            require ('gitsigns').setup() 
+        end
+    }
+
+    ----------------------------------------------------------------------------------------------
+    -- Languange Parser 
+    ----------------------------------------------------------------------------------------------
+    use {
+        'nvim-treesitter/nvim-treesitter',                     -- Language Parser, Indentation
+        run = ':TSUpdate'                                      -- Folding, Hightlighting Syntax
+    }
+
+    ----------------------------------------------------------------------------------------------
+    -- Language Server Protocol (LSP)
+    ----------------------------------------------------------------------------------------------
+    use {
+        'neovim/nvim-lspconfig',                               -- define protocol between server and neovim
+        'williamboman/nvim-lsp-installer',                     -- to make easier to install LSP
     }
 
     -- Automatically set up your configuration after cloning packer.nvim
