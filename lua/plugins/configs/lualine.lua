139ca186bce1589111aaa67b58f1890e6013cfe3
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-11 16:03:44 +0700

    refactor(configs): update lualine component condition to appear
    add check for buffer not empty, nvim-tree, toggle-term and nvim-gps

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,3 +1,37 @@
+local gps = require("nvim-gps")
+
+local function insideNvimTree()
+    return vim.bo.filetype == 'NvimTree'
+end
+
+local function insideToggleTerm()
+    return vim.bo.filetype == 'toggleterm'
+end
+
+local function bufferNotEmpty()
+    if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
+        return true
+    end
+
+    return false
+end
+
+-- to make sure component appear
+-- only when buffer ready and not inside nvim tree
+local function isComponentAppear()
+    return (
+        bufferNotEmpty()
+        and not insideNvimTree()
+    )
+end
+
+local function notAppearOnTerminal()
+    return (
+        not insideToggleTerm()
+        and isComponentAppear()
+    )
+end
+
 require('lualine').setup {
     options = {
         icons_enabled = true,
@@ -8,12 +42,69 @@ require('lualine').setup {
         always_divide_middle = true,
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_a = {
+            {
+                'mode',
+                cond = isComponentAppear
+            },
+            'test'
+        },
+        lualine_b = {
+            {
+                'branch',
+                cond = isComponentAppear
+            },
+            {
+                'diff',
+                cond = isComponentAppear
+            },
+            {
+                'diagnostics',
+                cond = isComponentAppear
+            }
+        },
+        lualine_c = {
+            {
+                'filename',
+                cond = notAppearOnTerminal
+            },
+            {
+                gps.get_location,
+                color = 'DevIconTxt',
+                cond = function()
+                    return (
+                        notAppearOnTerminal()
+                        and gps.is_available()
+                    )
+                end
+            },
+        },
+        lualine_x = {
+            {
+                'encoding',
+                cond = notAppearOnTerminal
+            },
+            {
+                'fileformat',
+                cond = notAppearOnTerminal
+            },
+            {
+                'filetype',
+                cond = notAppearOnTerminal
+            }
+        },
+        lualine_y = {
+            {
+                'progress',
+                cond = isComponentAppear
+            }
+        },
+        lualine_z = {
+            {
+                'location',
+                cond = isComponentAppear
+            }
+        }
     },
     inactive_sections = {
         lualine_a = {},
@@ -25,8 +116,8 @@ require('lualine').setup {
     },
     tabline = {
         lualine_a = {},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
+        lualine_b = {},
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
