54a621f2d7398fe98cef3eb2ccdc8ca7586c6ea6
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-12 15:22:38 +0700

    chore(utils): add nvim, neogit, diffview, telescope log to cleanup_vim.sh

--- utils/cleanup_nvim.sh
+++ utils/cleanup_nvim.sh
@@ -7,6 +7,17 @@ rm -rf ~/.local/share/nvim/*
 cat /dev/null > ~/.cache/nvim/lsp.log
 # Clean up lsp installer log
 cat /dev/null > ~/.cache/nvim/lsp-installer.log
+
 # Clean up packer.nvim.log
 cat /dev/null > ~/.cache/nvim/packer.nvim.log
 
+# Clean up nvim log
+cat /dev/null > ~/.cache/nvim/log
+
+# Clean up neogit.log
+cat /dev/null > ~/.cache/nvim/neogit.log
+# Clean up diffview.log
+cat /dev/null > ~/.cache/nvim/diffview.log
+
+# Clean up telescope.log
+cat /dev/null > ~/.cache/nvim/telescope.log
