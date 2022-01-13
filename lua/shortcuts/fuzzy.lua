d1ba5d8dfb25bfe857dec05945871077250e77a6
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-13 12:48:13 +0700

    refactor(shortcuts): add space as a prefix shortcuts for fuzzy opertion (file, git, etc operation)

--- lua/shortcuts/fuzzy.lua
+++ lua/shortcuts/fuzzy.lua
@@ -8,8 +8,8 @@ local map = require('utils.map')
 -- Find Files, Grep, Active Tab and Help config
 -------------------------------------------------------------------
 map {'n', '<C-p>', ':Telescope find_files<cr>'} -- ( Ctrl+p ) Find files
-map {'n', 'fif', ':Telescope live_grep<cr>'}    -- ( fif ) Find grep in files
-map {'n', 'ft', ':Telescope buffers<cr>'}       -- ( ft ) Find active tab
-map {'n', 'fh', ':Telescope help_tags<cr>'}     -- ( fh ) Find help
-map {'n', 'gs', ':Telescope git_status<cr>'}    -- ( gs ) Git status of active files
-map {'n', 'pr', ':Telescope projects<cr>'}      -- ( pr ) Explore available projects
+map {'n', '<space>fif', ':Telescope live_grep<cr>'}    -- ( fif ) Find grep in files
+map {'n', '<space>ft', ':Telescope buffers<cr>'}       -- ( ft ) Find active tab
+map {'n', '<space>fh', ':Telescope help_tags<cr>'}     -- ( fh ) Find help
+map {'n', '<space>gs', ':Telescope git_status<cr>'}    -- ( gs ) Git status of active files
+map {'n', '<space>pr', ':Telescope projects<cr>'}      -- ( pr ) Explore available projects
