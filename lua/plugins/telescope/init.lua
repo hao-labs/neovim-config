de2bf2c2d5f74772d53b235a8d36bd5e38d635aa
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-26 15:58:23 +0700

    style(tab-space): update tab-space to 2 spaces

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,34 +1,34 @@
 require('telescope').setup {
-    defaults = {
-        vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--trim" -- add this value
-        },
-        prompt_prefix = "  ",
-        sorting_strategy = "ascending",
-        winblend = 0,
-        layout_config = {
-            horizontal = {
-                prompt_position = "top",
-                preview_width = 0.55,
-                results_width = 0.8,
-            },
-            vertical = {
-                mirror = false,
-            },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-        },
-        file_ignore_patterns = {
-            "node_modules",
-            "storage"
-        },
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--trim" -- add this value
     },
+    prompt_prefix = "  ",
+    sorting_strategy = "ascending",
+    winblend = 0,
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    file_ignore_patterns = {
+      "node_modules",
+      "storage"
+    },
+  },
 }
