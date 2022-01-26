329e66159711ccafb3c8a9028558b666f75910cf
Cahyo Bagus L <hao.labs.2011@gmail.com>
2022-01-26 15:49:11 +0700

    refactor(tab): update tab spaces config from 4 to 2 spaces

--- lua/options/tabs.lua
+++ lua/options/tabs.lua
@@ -1,7 +1,8 @@
+local tab_spaces = 2
 -------------------------------------------------------------------
--- Tab 
+-- Tab
 -------------------------------------------------------------------
-vim.opt.tabstop = 4          -- number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4       -- number of spaces to use for each step of (auto) indent
-vim.opt.softtabstop = 4      -- same as tabstop, but its for editing operation
-vim.opt.expandtab = true     -- set whitespace as tab
+vim.opt.tabstop = tab_spaces          -- number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = tab_spaces       -- number of spaces to use for each step of (auto) indent
+vim.opt.softtabstop = tab_spaces      -- same as tabstop, but its for editing operation
+vim.opt.expandtab = true              -- set whitespace as tab
